60f919abd33479222797fea23cb70f29b16548f1
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-10 19:54:13 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
   def change
-    change_column :students, :birthdate, :datetime
+    change_column(:students, :birthdate, :integer
+    )
   end
 end
