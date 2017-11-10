856b1e5ec7213f124db485146ad9aa376f189280
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-10 19:36:47 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,5 +1,9 @@
 class CreateStudents < ActiveRecord::Migration
   def change
-
+    namespace :db do
+      task :migrate => :environment do
+        Student.create_table
+      end
+    end
   end
 end
